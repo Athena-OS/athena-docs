502c1cbc2580237ff47e64d6d4b4e0abd4b034e9
D3vil0p3r <>
2023-07-09 15:51:53 +0200

    Create Starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://athena-os.github.io',
+  base: '/athena-docs',
   integrations: [
     starlight({
       title: 'My Docs',
