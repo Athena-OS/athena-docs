e36cccda34dc2fe5ad0d339f77e41b434d583628
shard77 <sh7user@gmail.com>
2023-07-20 23:24:00 +0200

    optimized animation and made it faster

--- src/javascript/utils.js
+++ src/javascript/utils.js
@@ -4,38 +4,61 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const texts = ["InfoSec Professionals.", "Bug Bounty Hunters.", "Passionate Students.", "Spicy Hackers."];
   let textIndex = 0;
 
+  let isAnimating = false;
+  let rafID;
+  let then = Date.now();
+  const interval = 10;
+
+  const animate = () => {
+    isAnimating = true;
+    let iterations = 0;
+    const originalText = texts[textIndex];
+
+    const changeLetter = () => {
+      let now = Date.now();
+      let elapsed = now - then;
+
+      if (elapsed > interval) {
+        word.innerText = originalText
+          .split("")
+          .map((letter, index) => {
+            if (index < iterations) {
+              return originalText[index];
+            }
+            if (letter == " ") {
+              return " ";
+            }
+            return letters[Math.floor(Math.random() * 22)];
+          })
+          .join("");
+
+        iterations += 1 / 3;
+        then = now - (elapsed % interval);
+      }
+
+      if (iterations < word.innerText.length && isAnimating) {
+        rafID = requestAnimationFrame(changeLetter);
+      }
+    }
+
+    rafID = requestAnimationFrame(changeLetter);
+  }
+
   animate();
 
   setInterval(() => {
     textIndex = (textIndex + 1) % texts.length;
-    animate();
+    if (!document.hidden) {
+      animate();
+    }
   }, 4000);
 
-  function animate() {
-    let iterations = 0;
-    const originalText = texts[textIndex];
-    let timeout;
-
-    const changeLetter = () => {
-      word.innerText = originalText
-        .split("")
-        .map((letter, index) => {
-          if (index < iterations) {
-            return originalText[index];
-          }
-          if (letter == " ") {
-            return " ";
-          }
-          return letters[Math.floor(Math.random() * 22)];
-        })
-        .join("");
-
-      iterations += 1 / 3;
-      if (iterations < word.innerText.length) {
-        timeout = setTimeout(changeLetter, 30);
-      }
+  document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+      isAnimating = false;
+      cancelAnimationFrame(rafID);
+    } else {
+      animate();
     }
-
-    timeout = setTimeout(changeLetter, 30);
-  }
+  });
 });
