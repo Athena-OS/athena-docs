51ffef89268d58acf71e173dcad0447494859c9a
D3vil0p3r <vozaanthony@gmail.com>
2024-07-07 16:39:07 +0200

    Update website elements

---
+++ src/javascript/downloadsData.js
@@ -0,0 +1,73 @@
+export const data = [
+    {
+      title: "Installer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus velit ac rutrum iaculis. Aliquam imperdiet, ipsum sed tempor accumsan, enim nisi blandit arcu, eu scelerisque erat lacus vitae ante.",
+        image: "/src/assets/ISO.svg",
+      hashes: {
+        MD5: "7HFEJeGYbuXwkfMGy3yXAJhARt08gyzxCOgtDAJKiDpHNhnX2VA9LQ7O99a36734",
+        SHA1: "hg9qMB4356v24ERAn7Bx5dZoHuHNsQ57ibxlG0OH1JJUcBWbojA4aRNEuAFUvMmi",
+        SHA256:
+          "5eb9dc96cccbdfe7610d3cbced1bd6ee89b5acdfc83ffee1f06e6d02b058390c",
+        SHA512:
+          "abs118c0267216830b4214dcd2a2d2a3891a97dab6cf2aa5cd398262c0343d89a6beea759b9052a5cd37edccfdab855174a4ef0f56100177548d7c6f91bc0ab414d",
+      },
+    },
+    {
+      title: "Installer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus velit ac rutrum iaculis. Aliquam imperdiet, ipsum sed tempor accumsan, enim nisi blandit arcu, eu scelerisque erat lacus vitae ante.",
+        image: "/src/assets/vmware.svg",
+      hashes: {
+        MD5: "7HFEJeGYbuXwkfMGy3yXAJhARt08gyzxCOgtDAJKiDpHNhnX2VA9LQ7O99a36734",
+        SHA1: "hg9qMB4356v24ERAn7Bx5dZoHuHNsQ57ibxlG0OH1JJUcBWbojA4aRNEuAFUvMmi",
+        SHA256:
+          "5eb9dc96cccbdfe7610d3cbced1bd6ee89b5acdfc83ffee1f06e6d02b058390c",
+        SHA512:
+          "kFJrYlvyVlnqBMGpjRFNWskHzr1Bdv9VTOrqV9HogAspOWpCAzRDEsPiOMMJ1qle",
+      },
+    },
+    {
+      title: "Installer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus velit ac rutrum iaculis. Aliquam imperdiet, ipsum sed tempor accumsan, enim nisi blandit arcu, eu scelerisque erat lacus vitae ante.",
+        image: "/src/assets/vbox.svg",
+        hashes: {
+        MD5: "7HFEJeGYbuXwkfMGy3yXAJhARt08gyzxCOgtDAJKiDpHNhnX2VA9LQ7O99a36734",
+        SHA1: "hg9qMB4356v24ERAn7Bx5dZoHuHNsQ57ibxlG0OH1JJUcBWbojA4aRNEuAFUvMmi",
+        SHA256:
+          "5eb9dc96cccbdfe7610d3cbced1bd6ee89b5acdfc83ffee1f06e6d02b058390c",
+        SHA512:
+          "kFJrYlvyVlnqBMGpjRFNWskHzr1Bdv9VTOrqV9HogAspOWpCAzRDEsPiOMMJ1qle",
+      },
+    },
+    {
+      title: "Installer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus velit ac rutrum iaculis. Aliquam imperdiet, ipsum sed tempor accumsan, enim nisi blandit arcu, eu scelerisque erat lacus vitae ante.",
+        image: "/src/assets/docker.svg",
+        hashes: {
+        MD5: "7HFEJeGYbuXwkfMGy3yXAJhARt08gyzxCOgtDAJKiDpHNhnX2VA9LQ7O99a36734",
+        SHA1: "hg9qMB4356v24ERAn7Bx5dZoHuHNsQ57ibxlG0OH1JJUcBWbojA4aRNEuAFUvMmi",
+        SHA256:
+          "5eb9dc96cccbdfe7610d3cbced1bd6ee89b5acdfc83ffee1f06e6d02b058390c",
+        SHA512:
+          "kFJrYlvyVlnqBMGpjRFNWskHzr1Bdv9VTOrqV9HogAspOWpCAzRDEsPiOMMJ1qle",
+      },
+    },
+    {
+      title: "Installer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus velit ac rutrum iaculis. Aliquam imperdiet, ipsum sed tempor accumsan, enim nisi blandit arcu, eu scelerisque erat lacus vitae ante.",
+        image: "/src/assets/wsl.svg",
+        hashes: {
+        MD5: "7HFEJeGYbuXwkfMGy3yXAJhARt08gyzxCOgtDAJKiDpHNhnX2VA9LQ7O99a36734",
+        SHA1: "hg9qMB4356v24ERAn7Bx5dZoHuHNsQ57ibxlG0OH1JJUcBWbojA4aRNEuAFUvMmi",
+        SHA256:
+          "5eb9dc96cccbdfe7610d3cbced1bd6ee89b5acdfc83ffee1f06e6d02b058390c",
+        SHA512:
+          "kFJrYlvyVlnqBMGpjRFNWskHzr1Bdv9VTOrqV9HogAspOWpCAzRDEsPiOMMJ1qle",
+      },
+    },
+    // ... other items (you can repeat this structure for multiple installers)
+  ];
