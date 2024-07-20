f6754342d95bc74b79e5af741c58ee57c559988d
D3vil0p3r <crazysnob@live.it>
2024-07-20 12:39:42 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,262 +1,26 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://athenaos.org/',
-  integrations: [starlight({
-    title: 'Athena OS',
-    description: 'Athena OS, a new pentesting/cybersecurity linux distribution.',
-    components: {
-      TableOfContents: "./src/components/TOC.astro",
-      PageTitle: "./src/components/CustomPageTitle.astro"
-    },
-    logo: {
-      light: '/src/assets/athena-light.svg',
-      dark: '/src/assets/athena-dark.svg'
-    },
-    customCss: process.env.NO_GRADIENTS ? ['./src/styles/_global.css'] : ['./src/styles/landing.css', './src/styles/_global.css'],
-    social: {
-      github: 'https://github.com/Athena-OS',
-      instagram: 'https://www.instagram.com/athenaos_sec',
-      discord: 'https://discord.gg/athena-os-977645785170714644'
-    },
-    editLink: {
-      baseUrl: 'https://github.com/Athena-OS/athena-docs/tree/main/'
-    },
-    lastUpdated: true,
-    sidebar: [{
-      label: 'Getting Started',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Manifesto',
-        link: '/en/getting-started/manifesto'
-      }, {
-        label: 'Why Athena OS?',
-        link: '/en/getting-started/athenaos'
-      }, {
-        label: 'Downloading Athena OS',
-        link: '/en/getting-started/download'
-      }]
-    }, {
-      label: 'Installation',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Installation prepare',
-        link: '/en/installation/prepare-install'
-      }, {
-        label: 'Installing as single boot',
-        link: '/en/installation/install-single-boot'
-      }, {
-        label: 'Installing as dual boot with Linux or Windows',
-        link: '/en/installation/install-dual-boot'
-      }, {
-        label: 'Installing by NixOS',
-        link: '/en/installation/install-nixos'
-      }, {
-        label: 'Installing as persistent USB',
-        link: '/en/installation/install-persistent-usb'
-      }, {
-        label: 'Installing as guest in VMware',
-        link: '/en/installation/install-guest-vmware'
-      }, {
-        label: 'Installing as guest in VirtualBox',
-        link: '/en/installation/install-guest-virtualbox'
-      }, {
-        label: 'Installing as guest in Hyper-V',
-        link: '/en/installation/install-guest-hyperv'
-      }, {
-        label: 'Installing as guest in QEMU/KVM',
-        link: '/en/installation/install-guest-qemu'
-      }, {
-        label: 'Installing as guest in Parallels',
-        link: '/en/installation/install-guest-parallels'
-      }, {
-        label: 'Installing as guest in UTM',
-        link: '/en/installation/install-guest-utm'
-      }, {
-        label: 'Updating Athena OS',
-        link: '/en/installation/update-athena'
-      }]
-    }, {
-      label: 'Configuration',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Aegis Installer TUI',
-        link: '/en/configuration/aegis-tui'
+  integrations: [
+    starlight({
+      title: 'Athena OS',
+      description: 'Athena OS, a new pentesting/cybersecurity linux distribution.',
+      logo: {
+        light: '/src/assets/athena-light.svg',
+        dark: '/src/assets/athena-dark.svg',
       },
-      // { label: 'Calamares Installer', link: '/en/configuration/calamares' },
-      {
-        label: 'Kernel',
-        link: '/en/configuration/kernel'
-      }, {
-        label: 'Shell',
-        link: '/en/configuration/shell'
-      }, {
-        label: 'Display Manager',
-        link: '/en/configuration/display-manager'
-      }, {
-        label: 'GNOME',
-        link: '/en/configuration/gnome'
-      }, {
-        label: 'Bspwm',
-        link: '/en/configuration/bspwm'
-      }, {
-        label: 'KDE Plasma',
-        link: '/en/configuration/kde'
-      }, {
-        label: 'MATE',
-        link: '/en/configuration/mate'
-      }, {
-        label: 'Cinnamon',
-        link: '/en/configuration/cinnamon'
-      }, {
-        label: 'XFCE',
-        link: '/en/configuration/xfce'
-      }, {
-        label: 'Hyprland',
-        link: '/en/configuration/hyprland'
-      }, {
-        label: 'NVIDIA GPU',
-        link: '/en/configuration/nvidia'
-      }, {
-        label: 'Repositories',
-        link: '/en/configuration/repositories'
-      }, {
-        label: 'Utilities',
-        link: '/en/configuration/utilities'
-      }]
-    }, {
-      label: 'Cyber Resources',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Athena Welcome',
-        link: '/en/resources/athena-welcome'
-      }, {
-        label: 'Pentesting Tools',
-        link: '/en/resources/pentesting-tools'
-      }, {
-        label: 'Athena Cyber Hub',
-        link: '/en/resources/cyber-hub'
-      }, {
-        label: 'Hack The Box Toolkit',
-        link: '/en/resources/htb-toolkit'
-      }, {
-        label: 'PWNage Menu',
-        link: '/en/resources/pwnage-menu'
-      }, {
-        label: 'Blue Team Menu',
-        link: '/en/resources/blueteam-menu'
-      }, {
-        label: 'Red Team Menu',
-        link: '/en/resources/redteam-menu'
-      }, {
-        label: 'Payload to Dock',
-        link: '/en/resources/payload-dock'
-      }, {
-        label: 'Payload Environment Variables',
-        link: '/en/resources/payload-variables'
-      }, {
-        label: 'Browser Pentesting Resources',
-        link: '/en/resources/browser-pentesting'
-      }, {
-        label: 'NIST Feed',
-        link: '/en/resources/nist-feed'
-      }]
-    },
-    //{
-    //  label: 'Customization',
-    //  items: [
-    //    // Each item here is one entry in the navigation menu.
-    //    { label: 'Customizing Shells', link: '/en/customization/shell' },
-    //    { label: 'Customizing Display Managers', link: '/en/customization/display-manager' },
-    //    { label: 'Customizing Terminal Tools', link: '/en/customization/terminal-tools' },
-    //  ],
-    //},
-    {
-      label: 'Containers',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Athena OS Core Image',
-        link: '/en/containers/core-image'
-      }, {
-        label: 'Athena OS RDP Image',
-        link: '/en/containers/rdp-image'
-      }]
-    }, {
-      label: 'Windows Subsystem for Linux',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Athena OS WSL',
-        link: '/en/wsl/wsl'
-      }, {
-        label: 'Win-KeX',
-        link: '/en/wsl/win-kex'
-      }]
-    }, {
-      label: 'Support',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Troubleshooting',
-        link: '/en/support/troubleshooting'
-      }]
-    }, {
-      label: 'Community',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Contribute to Athena',
-        link: '/en/community/contribute'
-      }, {
-        label: 'Getting Help',
-        link: '/en/community/getting-help'
+      customCss: process.env.NO_GRADIENTS ? [	'./src/styles/_global.css'] : ['./src/styles/landing.css', 	'./src/styles/_global.css'],
+      social: {
+        github: 'https://github.com/Athena-OS',
+        instagram: 'https://www.instagram.com/athenaos_sec',
+        discord: 'https://discord.gg/athena-os-977645785170714644',
       },
-      //{ label: 'Official Athena OS Mirrors', link: '/en/community/mirrors' },
-      {
-        label: 'Style Guide',
-        link: '/en/community/style-guide'
-      }, {
-        label: 'Setting up a Mirror',
-        link: '/en/community/setting-mirror'
-      }, {
-        label: 'Submitting Bugs',
-        link: '/en/community/submitting-bugs'
-      }, {
-        label: 'Submitting Themes',
-        link: '/en/community/submitting-themes'
-      }, {
-        label: 'Submitting Tools in Nix',
-        link: '/en/community/submitting-tools-nix'
-      }, {
-        label: 'Submitting Tools in Arch',
-        link: '/en/community/submitting-tools-arch'
-      }]
-    }, {
-      label: 'Development',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Testing',
-        link: '/en/development/testing'
-      }]
-    }, {
-      label: 'Policy',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Athena OS EULA',
-        link: '/en/policy/eula-policy'
+      editLink: {
+        baseUrl: 'https://github.com/Athena-OS/athena-docs/tree/main/',
       },
-
       lastUpdated: true,
       sidebar: [
         {
@@ -398,12 +162,8 @@ export default defineConfig({
         },
       ],
     }),
-react()],
-  
+  ],
+
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp'
-    }
-  }
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
 });
