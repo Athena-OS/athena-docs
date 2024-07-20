15f9368023a1d07f00e9909872148773ff17ee21
D3vil0p3r <vozaanthony@gmail.com>
2024-07-20 15:09:35 +0200

    Update website

---
+++ src/javascript/builderData.js
@@ -0,0 +1,255 @@
+
+export const builderData=[
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "airgeddon-git v11.22.r0.gfe2c42b-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='fail'>Fail</span> v11.22.r0.gfe2c42b-1",
+    Statusx86_64: "<span class='done'>Done</span> v11.22.r0.gfe2c42b-1",
+    StatusARMv6h: "Incomplete v10.42.r0.g71a13d0-1",
+    StatusARMv7h: "Incomplete v10.42.r0.g71a13d0-1",
+    StatusAArch64: "Incomplete v10.42.r0.g71a13d0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "airgeddon-git v11.22.r0.gfe2c42b-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='fail'>Fail</span> v11.22.r0.gfe2c42b-1",
+    Statusx86_64: "<span class='done'>Done</span> v11.22.r0.gfe2c42b-1",
+    StatusARMv6h: "Incomplete v10.42.r0.g71a13d0-1",
+    StatusARMv7h: "Incomplete v10.42.r0.g71a13d0-1",
+    StatusAArch64: "Incomplete v10.42.r0.g71a13d0-1"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "airgeddon-git v11.22.r0.gfe2c42b-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='fail'>Fail</span> v11.22.r0.gfe2c42b-1",
+    Statusx86_64: "<span class='done'>Done</span> v11.22.r0.gfe2c42b-1",
+    StatusARMv6h: "Incomplete v10.42.r0.g71a13d0-1",
+    StatusARMv7h: "Incomplete v10.42.r0.g71a13d0-1",
+    StatusAArch64: "Incomplete v10.42.r0.g71a13d0-1"
+  },
+  {
+    PackageName: "aiengine 2.1.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 2.1.0-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Incomplete 2.1.0-1",
+    StatusARMv7h: "<span class='done'>Done</span> 2.1.0-1",
+    StatusAArch64: "<span class='done'>Done</span> 2.1.0-1"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  },
+  {
+    PackageName: "amass 4.2.0-1",
+    Repository: "archstrike",
+    Statusi686: "<span class='done'>Done</span> 9.1.117252-1",
+    Statusx86_64: "<span class='done'>Done</span> 2.1.0-1",
+    StatusARMv6h: "Skip",
+    StatusARMv7h: "Skip",
+    StatusAArch64: "Skip"
+  }
+]
