96545ac69a7ca2773e230c287e8e6a264d805ba1
ammakr <aakrambhutta@gmail.com>
2024-11-22 23:32:59 +0500

    change to fully dynamic for everything on builder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://athenaos.org/",
+  output: "static",
   integrations: [
     starlight({
       title: "Athena OS",
