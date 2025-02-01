bc236b41c40743a91c584549c826531f2b28c574
D3vil0p3r <vozaanthony@gmail.com>
2025-02-01 22:54:35 +0100

    Update VM

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -6,12 +6,12 @@ export const data = [
       "Get the full capabilities of your hardware! Achieve unparalleled performance and seamless integration with your hardware, ensuring a smooth and efficient computing experience.",
     image: "/icons/iso.svg",
     hashes: {
-      MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
-      SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
+      //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
+      //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
         "d741453e1ea3121b384ffcf99a11846284f4df320a3b8fcc2734a246218ade7d",
-      SHA512:
-        "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
+      //SHA512:
+      //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
     downloadURL: "https://github.com/Athena-OS/athena/releases/download/v23.11/athena-rolling-x86_64.iso",
     enableDownloadCounter: true,
@@ -24,14 +24,14 @@ export const data = [
       "Experience effortlessly! Ensure optimal performance and seamless integration. Take advantage of full feature set without modifying your primary system, providing a robust and efficient solution for your virtualization needs.",
     image: "/icons/vmware.svg",
     hashes: {
-      MD5: "Work In Progress",
-      SHA1: "Work In Progress",
+      //MD5: "Work In Progress",
+      //SHA1: "Work In Progress",
       SHA256:
-        "Work In Progress",
-      SHA512:
-        "Work In Progress",
+        "f49f37fb2ba52a7bdf33c4465adce94f6f61ad7009afc94612299710483d1387",
+      //SHA512:
+      //  "Work In Progress",
     },
-    downloadURL: "https://cms.athenaos.org/api/downloads/14",
+    downloadURL: "https://hub.athenaos.org/athena-images/rolling/athena-x86_64-vmware.ova",
     enableDownloadCounter: true,
     buttonLabel: "Download",
   },
@@ -42,14 +42,14 @@ export const data = [
       "Run seamlessly! Designed for smooth performance and full compatibility. Enjoy all the features without altering your main system, providing a flexible and efficient virtualization solution.",
     image: "/icons/vbox.svg",
     hashes: {
-      MD5: "Work In Progress",
-      SHA1: "Work In Progress",
+      //MD5: "Work In Progress",
+      //SHA1: "Work In Progress",
       SHA256:
-        "Work In Progress",
-      SHA512:
-        "Work In Progress",
+        "5c8c061ddf5bed63630b9c40230bcbba0af2e1dfa8967e6d94907a4772aa7895",
+      //SHA512:
+      //  "Work In Progress",
     },
-    downloadURL: "https://cms.athenaos.org/api/downloads/16",
+    downloadURL: "https://hub.athenaos.org/athena-images/rolling/athena-x86_64-vbox.ova",
     enableDownloadCounter: true,
     buttonLabel: "Download",
   },
