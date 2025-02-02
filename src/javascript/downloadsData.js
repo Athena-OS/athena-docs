196e7a9eedd41180f548f8de85a3c748d412949e
D3vil0p3r <vozaanthony@gmail.com>
2025-02-02 22:03:38 +0100

    Update Hyprland

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,7 +9,7 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "d741453e1ea3121b384ffcf99a11846284f4df320a3b8fcc2734a246218ade7d",
+        "d4e6cf0fb21ec79478633c3a268b204ba1196d55ad6c0cd680deb5a6ef0b35c1",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
