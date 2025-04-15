f3138c49bf9d26c3bd96981080c2b0f89ed70d33
Antonio <crazysnob@live.it>
2025-04-15 09:38:44 +0200

    Update downloadsData.js

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,11 +9,11 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "8ffdccb7850ca948067d4423884c8da09120043b265b51c316abebc66f0a941a",
+        "4b6a8eba4da0e57dfa7ed205a3ea6c6e6d8dd55bb9e4ac880d11d8788168173a",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
-    downloadURL: "https://github.com/Athena-OS/athena/releases/download/v23.11/athena-rolling-x86_64.iso",
+    downloadURL: "https://hub.athenaos.org/athena-images/rolling/athena-rolling-x86_64.iso",
     enableDownloadCounter: true,
     buttonLabel: "Download",
   },
