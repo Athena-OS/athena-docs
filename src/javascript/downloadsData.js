9cb008ba11244b3ab84b17991e7ee3f52b32000f
Antonio <crazysnob@live.it>
2025-06-03 21:11:40 +0200

    Update downloadsData.js

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,7 +9,7 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "a19aa29236532585aa4363d69546396ae60e3c09d8c09a35eac0d4c9c9573aae",
+        "cbface4ebf5608cbe2f9e764dd35aa65e59e3486e02322930aa21ddf7b7b52bc",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
