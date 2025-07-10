21eb2d1be4aa1f7c3b48fca1299aa9fdce005697
Ammar Akram <73212302+ammakr@users.noreply.github.com>
2025-07-11 01:06:08 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: "https://athenaos.org/",
-  output: "static",
+  output: "server",
   adapter: netlify(),
   integrations: [
     starlight({
