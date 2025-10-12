c1ad5edc4039a7e7353ff5e1ed0500ec6e2ff861
Antonio <crazysnob@live.it>
2025-10-12 23:37:49 +0200

    Update downloadsData.js

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,7 +9,7 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "e1bd8c0fe421b18115f5c1092bdd8c5c6db3db38258ccc03d6122c47bd2668d1",
+        "a982275370c4ff1ad04eed91597317762e02079023d956e5e2d62ab1f6188478",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
@@ -77,6 +77,7 @@ export const data = [
     buttonLabel: "Get it now",
   },
 ];
+
 
 
 
