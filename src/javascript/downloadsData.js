910df913ee16e32aa70951d4368070106b3c8cde
expandpi <>
2025-11-04 04:36:55 +0100

    Update files

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,7 +9,7 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "08b2ff473f3d3a7b0b3f387d5d7ae9679ac64c6a231ca252c19849df81ff95a9",
+        "162fa904c0b5211743bbebd0c40d1026636b127999af60af903eff731ff2d8af",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
