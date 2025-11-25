a1a688af4baf682308115c3033991cb7dc1c25fd
D3v <crazysnob@live.it>
2025-11-25 03:36:42 +0100

    Update files

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,7 +9,7 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "95fa6519c0177e4d41c26e5ff957f1dd1584ebe3c17785bd9524bb7c5c696244",
+        "17dfb94b51ef9e24658fa0bde6cec2f1010740281cdc4729735fb33ee6b5a3c0",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
