890a6198db947462508a17251001e39a94707acf
Antonio <crazysnob@live.it>
2026-03-19 20:38:18 +0100

    Update downloadsData.js with new hash and cleanup
    Updated the SHA256 hash for the ISO Image entry and removed commented-out sections for VMware and VirtualBox images.

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,7 +9,7 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "4e7290bdfe56ed3a9221a2a3b7889d7121897fdb6e2223c320fc817428770c79",
+        "08149a3f63e5c47950a07aa3148d85de47ecd389ad13ef14109b1dfb2118cfd8",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
