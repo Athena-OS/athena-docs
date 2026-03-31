c25b9e6ea99289400b7cad0e798cc9688b11b884
D3vil0p3r <crazysnob@live.it>
2026-03-31 02:05:04 +0200

    Update hashes for ISO and Docker images
    Updated SHA256 hash for ISO Image and Docker Images.

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,7 +9,7 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "08149a3f63e5c47950a07aa3148d85de47ecd389ad13ef14109b1dfb2118cfd8",
+        "197758d2b8d32614a36acb7ddcb35357ba647ba5b19b01bfe862a5e68b7a3a3d",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
