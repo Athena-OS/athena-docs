a68ae9054b0445e3524ed4f8f98b019f2d9fca30
D3vil0p3r <crazysnob@live.it>
2026-04-07 23:05:41 +0200

    Fix Astro6 adapter issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: "https://athenaos.org/",
+  output: "server",
   adapter: netlify(),
   integrations: [
     starlight({
