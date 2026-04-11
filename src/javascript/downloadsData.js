bf1a7f6278dc4aa5e99ce2810727b0b968e5c910
D3vil0p3r <crazysnob@live.it>
2026-04-11 21:52:17 +0200

    Update downloadsData.js

--- src/javascript/downloadsData.js
+++ src/javascript/downloadsData.js
@@ -9,7 +9,7 @@ export const data = [
       //MD5: "43cee34b1d19fdf869bce7c67a57c9ec",
       //SHA1: "4d8a83f0a93f765c375cd735bb838edfd3381165",
       SHA256:
-        "a500febce97d18e09ea172247ebfebfdc11cfacc84b32ff3a3c933749127f327",
+        "2c0dc4e8b53dff5d3e6122cecf3979804a280bd82db23eadf7edb297d4649a27",
       //SHA512:
       //  "d5a1d67c7be9f11a4dd955caed75098d0c987aae7f7b74f85b972e3f2f0d1763e003fe40dd55851b24b0f726793058efad20700fde26b3dc57e0eb45c797830e",
     },
